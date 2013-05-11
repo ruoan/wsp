bfab5ff367e11db1044a9b6f912d3e167a2effce
Ruoan Xu <s_ruoan@hotmail.com>
2013-05-12 00:56:20 +0900

    update

--- app/webroot/js/common.js
+++ app/webroot/js/common.js
@@ -2,5 +2,40 @@ $(function(){
 	
 	//Tooltip
 	$('.tip').tooltip({placement:'bottom'});
+	$('.user_menu').profiledrop();
 	
 });
+
+//プロフィールのドロップダウンイベント
+$.fn.profiledrop = function(config){
+	//デフォルト設定
+	var hover_flg = false;
+	var self = this;
+	var defaults = {
+		speed : "fast",
+	}
+	
+	//configと結合
+	var options = $.extend(defaults, config);
+	
+	//プロフィールのクリックイベント
+	$('.user_profile', this).click(function(){
+		$(self).children('ul').toggle(options.speed);
+	});
+	
+	//マウスオーバー状態フラグセット
+	$(this).hover(
+	function(){
+		hover_flg = true;	
+	},
+	function(){
+		hover_flg = false;
+	});
+	
+	//html要素全体へのクリックイベント
+	$('html').click(function(){
+		if(!hover_flg){
+			$(self).children('ul').hide(options.speed);
+		}
+	});
+}

36905be58a192ebca760623b822454017ba4111e
Hirotake Sakai <HSakai@HSakai.local>
2013-07-02 03:37:12 +0900

    update

--- app/Model/SocialAccount.php
+++ app/Model/SocialAccount.php
@@ -28,46 +28,36 @@ class SocialAccount extends AppModel {
 	public function updateInsertAccount($token, $token_secret, $account_info){
 		
 		//登録用データを編集
-		//token
+		//token: IDとして使用
 		$member_data['token']= $token;
-		//
+		//secret: パスワードとして使用
 		$member_data['secret']= $secret;
-		//
+		//ログイン元サイトの種類: Twitterでは"TW"
 		$member_data['social_type']= Configure::read('Twitter.social_type');
-		//
+		//フルネーム
 		$member_data['name']= $account_info['name'];
-		//
+		//スクリーンネーム
 		$member_data['screen_name']= $account_info['screen_name'];
-		//
+		//プロフィール用画像URL
 		$member_data['profile_image_url']= $account_info['profile_image_url'];
-		//
+		//アカウントURL
 		$member_data['url']= $account_info['url'];
-		//
+		//e-mailアドレス
 		$member_data['email']= $account_info['email'];
-		
-		
+
+		//引数からのtokenがDBへ登録済みの既存ユーザーかを確認
 		$account_exsisting_data = $this->findByToken($token);
+		//DBが取得できた場合（既存ユーザーの場合）
 		if($account_exsisting_data){
+			//取得したIDを更新データへセット
 			$member_data['id'] = $account_exsisting['SocialAccount']['id'];
-			
+		//DBが取得できない場合（新規ユーザーの場合）	
 		}else{
-			
-			
-			
+			//SocialAccountのIDの最大値を取得し+1を新規IDとして
+			$new_id_number = $this->find('first', array("fields" => "MAX(SocialAccount.id) as max_number"));
+			$member_data['id'] = $new_id_number + 1;
 		}
 		$this->create();
 		$this->save(Array("SocialAccount" => $member_data));
-		
-		
-		
 	}
-	
-	
-	
-	
-	
-	
-	
-	
-	
 }

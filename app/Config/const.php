00a632c769d0a0a69552e6ec2358844ca92b754c
Ruoan Xu <s_ruoan@hotmail.com>
2014-04-13 21:01:24 +0900

    no message

--- app/Config/const.php
+++ app/Config/const.php
@@ -7,8 +7,8 @@
  * Twitter API
  */
 $config['Twitter'] = array(
-    'consumer_key'          => 'b7crCjiIs1pHYwK1e1i21A',
-    'consumer_secret'       => 'pcG4pWnxzTnj2eEndgKek7XWYbjxgMpIaQxWbr0gqs',
+    'consumer_key'          => '',
+    'consumer_secret'       => '',
     'social_type'			=> 'TW'
 );
 
@@ -16,8 +16,8 @@ $config['Twitter'] = array(
  * Facebook API
  */
 $config['Facebook'] = array(
-    'app_id'        => '296251410509103',
-    'app_secret'    => '4d5c7b8a532fd03eb0f0f20306c962a3',
+    'app_id'        => '',
+    'app_secret'    => '',
     'social_type'	=> 'FB'
 );
 
@@ -25,8 +25,8 @@ $config['Facebook'] = array(
  * Amazon Web Service
  */
 $config['Amazon'] = array(
-	'access_key_id'         => 'AKIAIX5UULELFQZ3ELTA',
-	'secret_access_key'     => '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',
+	'access_key_id'         => '',
+	'secret_access_key'     => '',
 	'associate'             => array(
 		'id'            => ' wsp0e-22',
 		'country_code'        => 'co.jp'
